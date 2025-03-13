994bf2c3d249e2b6eb98737b455402a995789fc8
solo925 <solomononyango925@gmail.com>
2025-03-13 12:21:23 +0300

    navbar fix

--- animehub/src/layout/Navbar.tsx
+++ animehub/src/layout/Navbar.tsx
@@ -1,6 +1,7 @@
-import { AppBar, Toolbar, IconButton, Typography, Container, Box } from "@mui/material";
+import { AppBar, Toolbar, IconButton, Typography, Container, Box} from "@mui/material";
 import { Brightness4, Brightness7 } from "@mui/icons-material";
 import { useThemeContext } from "../contexts/ThemeContext";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const { toggleTheme, darkMode } = useThemeContext();
@@ -10,7 +11,8 @@ const Navbar = () => {
       <AppBar position="fixed" sx={{ width: "100vw" }}>
         <Container maxWidth="xl" disableGutters>
           <Toolbar>
-            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component={Link} to="/"  sx={{ flexGrow: 1, textDecoration: "none", color: "inherit", cursor: "pointer" }}>
+
               AnimeHub
             </Typography>
             <IconButton onClick={toggleTheme} color="inherit">
